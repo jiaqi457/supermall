2d19f732c2f6a313562bab7527c893ddb4f766f8
‘jiaqi457 <2676263493@qq.com>
2019-09-27 11:57:32 +0800

    提交项目基本结构

---
+++ src/views/home/Home.vue
@@ -0,0 +1,114 @@
+<template>
+  <div id="home">
+    <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+    <home-swiper :banners="banners"></home-swiper>
+    <recommend-view :recommends="recommends"/>
+    <feature-view/>
+    <tab-control :titles="['流行','新款','精选']" class="tab-control"/>
+    <ul>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  import NavBar from 'components/common/navbar/NavBar'
+  import HomeSwiper from './childComps/HomeSwiper'
+  import RecommendView from './childComps/HomeRecommendView'
+  import FeatureView from './childComps/FeatureView'
+  import TabControl from 'components/content/tabControl/TabControl'
+
+  import {getHomeMultidata} from 'network/home'
+
+    export default {
+      name: "Home",
+      components:{
+        NavBar,
+        HomeSwiper,
+        RecommendView,
+        FeatureView,
+        TabControl
+      },
+      data(){
+        return{
+          banners:[],
+          recommends:[]
+        }
+      },
+      created(){
+        getHomeMultidata().then(res => {
+          this.banners = res.data.banner.list;
+          this.recommends = res.data.recommend.list;
+        })
+      }
+    }
+</script>
+
+<style scoped>
+  #home{
+    padding-top: 44px;
+  }
+  .home-nav{
+    background-color:var(--color-tint) ;
+    color: white;
+    position: fixed;
+    top: 0;
+    z-index: 1;
+    left: 0;
+    right: 0;
+  }
+  .tab-control{
+    position: sticky;
+    top: 44px;
+  }
+</style>
